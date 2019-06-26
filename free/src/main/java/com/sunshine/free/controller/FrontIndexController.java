8a0314ac09eb07a8d1289b34fb1ce4dbe04acd4d
OnTheWay6 <178003026@qq.com>
2019-06-26 16:57:07 +0800

    2019-6-26

--- free/src/main/java/com/sunshine/free/controller/FrontIndexController.java
+++ free/src/main/java/com/sunshine/free/controller/FrontIndexController.java
@@ -1,4 +1,25 @@
 package com.sunshine.free.controller;
 
+import com.sunshine.free.entity.User;
+import com.sunshine.free.utils.CommonResponse;
+import com.sunshine.free.utils.ResponseTypeEnums;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Free
+ * @date 2019-06-26
+ */
+
+@Controller(value="/index")
 public class FrontIndexController {
+
+    @RequestMapping(value = "/getProject", method =RequestMethod.POST)
+    public @ResponseBody CommonResponse<User> getProject(User user){
+
+        System.out.println(user);
+        return new CommonResponse<User>(ResponseTypeEnums.SUCCESS,null,null,user);
+    }
 }
